d58ee74274f831d1b3f0997067c8a57e494a63f4
arybkin <arybkin@tmxpub.agiotg.com>
2022-07-07 01:28:58 +0300

    add tabs

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// let tab = document.querySelector('.tab-item');
+
+// tab.addEventListener('click', function(){
+//     this.classList.toggle('active');
+//     let tabContent = this.children[1];
+//     let tabContentChildren = this.children[1].children;
+//     tabContentChildren = [...tabContentChildren];   
+
+
+//     tabHeights = tabContentChildren.map((item) =>{
+//         return item.clientHeight
+//     })
+
+//     tabHeights = tabHeights.reduce((prewVal, curVal) => prewVal + curVal);
+//     console.log(tabContent);
+    
+    
+// })
