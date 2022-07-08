e1885d63f52763d6cce4324a82b9277407ebe1e3
arybkin <arybkin@tmxpub.agiotg.com>
2022-07-08 06:00:51 +0300

    add tabs function

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,29 @@
-// let tab = document.querySelector('.tab-item');
+function startTabs() {
+  let tab = document.querySelectorAll(".tab-item");
+  tab = [...tab]
 
-// tab.addEventListener('click', function(){
-//     this.classList.toggle('active');
-//     let tabContent = this.children[1];
-//     let tabContentChildren = this.children[1].children;
-//     tabContentChildren = [...tabContentChildren];   
+  tab.forEach((item) => {    
+    item.addEventListener("click", function () {
+        clearHeight(tab);
+      this.classList.toggle("active");
+      let panel = this.querySelector(".tab__content");
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  });
+
+  function clearHeight(items){
+    items.map(item => {
+        item.classList.remove('active');
+        let panel = item.querySelector(".tab__content");
+        panel.style.maxHeight = null;
+    })
+  }
+}
 
 
-//     tabHeights = tabContentChildren.map((item) =>{
-//         return item.clientHeight
-//     })
 
-//     tabHeights = tabHeights.reduce((prewVal, curVal) => prewVal + curVal);
-//     console.log(tabContent);
-    
-    
-// })
+startTabs();
